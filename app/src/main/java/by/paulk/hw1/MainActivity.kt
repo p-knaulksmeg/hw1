45badab692d09b1234be5eb83929558523fa8f64
Paul <pavelkeytof@gmail.com>
2019-03-15 03:22:39 +0300

    "hello" toast for emulate mr conflict

--- app/src/main/java/by/paulk/hw1/MainActivity.kt
+++ app/src/main/java/by/paulk/hw1/MainActivity.kt
@@ -10,10 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
-=======
-        Toast.makeText(this, "bye-bye", Toast.LENGTH_SHORT).show()
->>>>>>> feature2
+
     }
 }
